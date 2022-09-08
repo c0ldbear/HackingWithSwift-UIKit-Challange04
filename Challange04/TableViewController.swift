f00c79288f302f4333c763099d5e5aae2f1f1d43
teddy <teddyjh@protonmail.com>
2022-09-08 13:06:09 +0200

    Add 5 rows with title Photo #n where n={1,5}

--- Challange04/TableViewController.swift
+++ Challange04/TableViewController.swift
@@ -13,6 +13,15 @@ class TableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath)
+        cell.textLabel?.text = "Photo #\(indexPath.row + 1)"
+        return cell
+    }
+    
 }
 
