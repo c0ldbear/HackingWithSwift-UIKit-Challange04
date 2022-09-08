352d708a71fc6f7e5318005e49b1481cca33ac07
teddy <teddyjh@protonmail.com>
2022-09-08 13:08:08 +0200

    Add detail view for photo: PhotoViewController

---
+++ Challange04/PhotoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoViewController.swift
+//  Challange04
+//
+//  Created by teddy on 2022-09-08.
+//
+
+import Foundation
+import UIKit
+
+class PhotoViewController: UIViewController {
+    var caption: String = ""
+    var image: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
