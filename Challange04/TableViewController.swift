7282a85a13d44275642b6423aa30d56f78fae403
teddy <teddyjh@protonmail.com>
2022-09-08 13:00:17 +0200

    Add navcontroller, change vc -> table view controller

--- Challange04/TableViewController.swift
+++ Challange04/TableViewController.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
